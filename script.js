0acae95170bac47039ba11b9d0ac1fcc21d5222a
Brandon Pena <brandonpena@gmail.com>
2021-11-18 17:11:38 -0500

    fixing question appearance

--- script.js
+++ script.js
@@ -16,10 +16,24 @@ function displayQuestion3 () {
     questionSection3.style.display ="block" ;
 }
 
+function displayQuestion4 () {
+    var questionSection4 = document.querySelector("#question-section4");
+    questionSection4.style.display ="block";
+}
+
+function displayQuestion5 () {
+    var questionSection5 = document.querySelector("#question-section5")
+    questionSection5.style.display="block";
+}
+
 var startButton = document.querySelector("#start-btn");
 var nextQuestion = document.querySelector("#next-question")
 var nextQuestion2 = document.querySelector("#next-question2")
+var nextQuestion3 = document.querySelector("#next-question3")
+var nextQuestion4 = document.querySelector("#next-question4")
 
 startButton.addEventListener("click", displayQuestion)
 nextQuestion.addEventListener("click", displayQuestion2 )
 nextQuestion2.addEventListener("click", displayQuestion3 )
+nextQuestion3.addEventListener("click",displayQuestion4)
+nextQuestion4.addEventListener("click",displayQuestion5)
